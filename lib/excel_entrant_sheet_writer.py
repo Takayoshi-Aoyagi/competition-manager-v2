dff98c71bb06c77979afb8f9b9cd68a5e027fd20
Takayoshi Aoyagi <takayoshi_aoyagi@waku-2.com>
2018-05-16 19:19:43 +0900

    add event sheets

--- lib/excel_entrant_sheet_writer.py
+++ lib/excel_entrant_sheet_writer.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import collections
+
 from excel_writer import ExcelWriter
 
 class ExcelEntrantSheetWriter:
@@ -14,16 +16,30 @@ class ExcelEntrantSheetWriter:
             row.append(entrant[key])
         return row
 
-    def _create_all_entrants_sheet_data(self):
+    def _create_sheet_data(self, entrants):
         rows = []
-        for entrant in self.entrants:
+        for entrant in entrants:
             row = self._get_all_entrant_row_data(entrant)
             rows.append(row)
         return rows
 
+    def _get_uniq_data(self, column_name):
+        keys = list(set(map(lambda x: x[column_name], self.entrants)))
+        keys.sort()
+        return keys
+    
+    def _create_event_sheet_data(self, data, prefix, key_name):
+        keys = self._get_uniq_data(key_name)
+        for key in keys:
+            _entrants = filter(lambda x: x[key_name] == key, self.entrants)
+            sheet_name = prefix + " " + key
+            data[sheet_name] = self._create_sheet_data(_entrants)
+        
     def _create_data(self):
-        data = {}
-        data[u'参加者一覧'] = self._create_all_entrants_sheet_data()        
+        data = collections.OrderedDict()
+        data[u'参加者一覧'] = self._create_sheet_data(self.entrants)
+        self._create_event_sheet_data(data, 'M', 'massogi')
+        self._create_event_sheet_data(data, 'T', 'tul')
         return data
         
     def write(self, file_path):
